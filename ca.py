0a6d1c3d80c2354ca793ad988c088fca063af9f6
TilakBabuTR <tilakbabu.bopparaju@thomsonreuters.com>
2020-06-13 20:02:23 +0530

    Modified accordingly

--- ca.py
+++ ca.py
@@ -15,14 +15,23 @@ content = soup.find_all('div', class_='q-set')
 title = soup.find_all('h3', class_='MdStoryHeadsTel')
 file_name=title[0].text
 
-# for tags in content:
-#     ques = tags.find_all("strong")
-#     print(ques[0].text)
+# for data in content:
+#     ques = data.find("p")
+#     answ = data.find("li")
+#     if (ques is not None):
+#         print(str(ques.get_text(strip=True)),end="\n\n")
+#     if (answ is not None):
+#         print(str(answ.get_text(strip=True)))
+#         # print(str(ques.get_text(strip=True)),end="\n\n")
 
 with open('./{}.txt'.format(file_name),'wt', encoding='utf-8') as file:
     for data in content:
-        ques = data.find_all(["strong","li","p"])
-        file.write(str(ques[0].get_text("\n",strip=True)))
-        file.write("\n")
-        # file.write(re.sub(r'\s*:\s*', ':', ques).strip()+"\n")
+        ques = data.find("p")
+        answ = data.find("li")
+        if (ques is not None):
+            file.write(str(ques.get_text("\n\n",strip=True)))
+            file.write("\n\n")
+        if (answ is not None):
+            file.write(str(answ.get_text(strip=True)))
+            file.write("\n\n")
 print("file created successfully")
